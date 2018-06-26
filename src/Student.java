11bfe5550d17c24dbf9c40f15bde16fe0fee6425
Aligator_sah <saixiongh@outlook.com>
2018-06-26 17:39:26 -0400

    first commit

---
+++ src/Student.java
@@ -0,0 +1,15 @@
+public class Student {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public void displayInfo() {
+        System.out.println("Hello: " + name);
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,15 @@
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class Test {
+    public static void main(String[] args) {
+        Resource resource = new ClassPathResource("spring-config.xml");
+        BeanFactory factory = new XmlBeanFactory(resource);
+        Student student = (Student)factory.getBean("studentbean");
+        Employee employee = (Employee)factory.getBean("employeebean");
+        student.displayInfo();
+        employee.show();
+    }
+}
